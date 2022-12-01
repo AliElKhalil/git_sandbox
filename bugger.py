13dab3b65633fad5b38fffac79f8bc36dce24c11
random <random@ipsa.fr>
2022-12-01 10:38:11 +0000

    I'm back

--- bugger.py
+++ bugger.py
@@ -1,8 +1,9 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
 def plot_sinus():
-    import numpy as np
-    import matplotlib.pyplot as plt
+  angle = np.arange(360)
+  sinus = np.sin(2 * np.pi * angle /360)
 
-    angle = np.arange(360)
-    sinus = np.sin(2 * np.pi * angle /360)
-
-    plt.plot(angle, sinnus)
+  plt.plot(angle, sinus)    
+  
